1e9c2d2cd860befbf1ff9563c7e69563926f65a4
Ivoski <skorskiivo@gmail.com>
2022-11-29 21:04:03 -0300

    Subida para correccion

---
+++ js/app.js
@@ -0,0 +1,182 @@
+
+// function loginadmin(){
+//     let nombreadmin = prompt ("Ingrese su nombre de administrador")
+//     if (nombreadmin == "Ivoski"){
+//         alert("¡Hola, Ivoski!");
+//         sacarturno()
+//     }else{
+//         alert("You not Ivoski, Get out here!");
+//     }
+
+// }
+
+// function sacarturno(){
+//     for (let i = 1; i <= 5; i++) {
+//         let ingresarNombre = prompt ("Ingresar Nombre para sacar un turno");
+//         alert("Turno N°"+i+"Nombre: "+ingresarNombre);
+//     }
+// }
+
+// loginadmin();
+
+// class Producto {
+//     constructor(nombre, precio) {
+//         this.nombre  = nombre;
+//         this.precio  = parseFloat(precio);
+//         this.stock = true;
+//     }
+//     sumaIva() {
+//         return(this.precio * 1.21);
+//     }
+// }
+
+// const productos = [];
+// productos.push(new Producto("Shampoo", 370));
+// productos.push(new Producto("Polvo para textura", 400));
+// productos.push(new Producto("Tintura", 700));
+
+// for (const producto of productos){
+//     console.log("------------")
+//     console.log(producto.nombre)
+//     console.log(producto.precio)
+// }
+
+
+// EVENTOS Y DOM
+let carrito = JSON.parse(localStorage.getItem("carrito")) || []
+
+const contenedorProductos = document.querySelector(`#contenedor-productos`)
+const contenedorCarrito = document.querySelector(`#carrito-contenedor`)
+const contadorCarrito = document.querySelector(`#contadorCarrito`)
+const contadorPrecioTotal = document.querySelector(`#precioTotal`)
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    if (localStorage.getItem('carrito')) {
+        renderCarrito()
+    }
+})
+
+
+stockProductos.forEach((producto) => {
+
+    const div = document.createElement('div')
+    div.className = 'producto'
+
+    div.innerHTML = `
+            <img src=${producto.img} alt="">
+            <h3>${producto.nombre}</h3>
+            <p>${producto.desc}</p>
+            <p class="precioProducto">Precio: $${producto.precio}</p>
+    `
+
+    const button = document.createElement('button')
+    button.className = "boton-agregar"
+    button.innerHTML = `Agregar <i class="fas fa-shopping-cart"></i>`
+
+    button.addEventListener('click', () => {
+        agregarAlCarrito(producto.id)
+    })
+
+    div.append(button)
+
+    contenedorProductos.append(div)
+})
+
+// Agregar productos al carrito 
+const agregarAlCarrito = (id) => {
+    const producto = stockProductos.find( (item) => item.id === id )
+    carrito.push(producto)
+    
+    Swal.fire({
+        icon:'success',
+        title: 'el producto fue agregado exitosamente',
+        toast: true,
+        timer: 1500,
+        showConfirmButton: false,
+        position: 'bottom-left' 
+    })
+
+    saveLocal()
+    console.log(carrito)
+    renderCarrito()
+}
+
+
+
+
+const renderCarrito = () => {
+    renderListadoCarrito()
+    renderCantidadCarrito()
+    renderTotalCarrito()
+}
+
+const renderListadoCarrito = () => {
+    contenedorCarrito.innerHTML = ''
+
+    carrito.forEach((producto) => {
+        const div = document.createElement('div')
+        div.className = "productoEnCarrito"
+        div.innerHTML = `
+            <p>${producto.nombre}</p>
+            <p>Precio: $${producto.precio}</p>
+            
+        `
+        
+        contenedorCarrito.append(div)
+    })
+}
+
+//Contador total de carrito
+
+const renderCantidadCarrito = () => {
+    const carritoLength = carrito.length;
+    localStorage.setItem("carritoLength", JSON.stringify(carritoLength))
+    contadorCarrito.innerText = JSON.parse(localStorage.getItem("carritoLength"));
+}
+
+const renderTotalCarrito = () => {
+    let total = 0
+
+    carrito.forEach((producto) =>{
+        total += producto.precio
+    })
+
+    contadorPrecioTotal.innerText = total
+}
+
+// Set item
+const saveLocal = () => {
+localStorage.setItem("carrito",JSON.stringify(carrito));
+};
+
+
+// vaciar carrito (Realizado con Sweet Alert)
+
+const btnVaciar = document.querySelector(`#vaciarCarrito`)
+
+btnVaciar.addEventListener('click', () => {
+    Swal.fire ({
+        title: '¿Está seguro?',
+        text: 'Esta accion no es reversible',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonText: 'Sí, Estoy seguro',
+        cancelButtonText: 'Dejame Pensarlo'
+    }).then((result)     => {       
+
+        if (result.isConfirmed) {
+            Swal.fire({
+                title: 'Correctamente Eliminado',
+                icon: 'success',
+                confirmButtonText: 'Volver a Comprar',
+                timer: 1500
+            }) 
+            
+            carrito.length = 0
+            renderCarrito()
+           
+        }
+    }
+    )
+})
---
+++ js/stock.js
@@ -0,0 +1,6 @@
+let stockProductos = [
+    { id: 1, nombre: "Cera para Peinar", tipo: "Ceras", desc: "Cera para peinar unisex", precio: 2099, img: '../img/cera1.jpg' },
+    { id: 2, nombre: "Cera para peinar", tipo: "Ceras", desc: "Cera para peinar para Hombre", precio: 2199, img: '../img/cera2.jpg' },
+    { id: 3, nombre: "Shampoo Matizador", tipo: "Shampoo", desc: "Shampoo para cabellos teñidos", precio: 3499, img: '../img/sm.jpg' },
+    { id: 4, nombre: "Polvo texturizador", tipo: "Polvos", desc: "Para que tu pelo tenga una textura unica", precio: 1999, img: '../img/ppp.jpg' },
+]
